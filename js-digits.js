17b84673bbe3235dd0f25ba340019bae21d50642
Murvat <murvat.mustafayev@mail.ru>
2023-11-09 12:36:47 +0100

    Rename branch to main

---
+++ js-digits.js
@@ -0,0 +1,22 @@
+let number= 5;
+let num2 = 2.5;
+let num3 = .5;
+let exp = 2e5;
+let expSM = 2e-3;
+expFract = 1.25e3;
+expFr3 = 1.25e-3;
+
+let dec = 9;
+let b = 0b1001;
+let oct = 0o10;
+let x = 0x1b;
+
+
+
+let x2 = 10 + 5;
+let y = 10-5;
+let u = 10/5;
+let t = 10 % 2;
+
+console.log(t++);
+console.log(t);
