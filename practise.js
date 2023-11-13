be6246479361706ce37233bd330d8aeace77474e
Murvat <122210081+Murvat@users.noreply.github.com>
2023-11-13 17:37:47 +0100

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,22 @@
+let entrances = 4;
+let floors = 9;
+let flatsPerFloor = 4;
+
+let flatPerEntrance =  floor * flatsPerFloor;
+console.log('Kvartir v podyezde', flatsPerEntrance);
+
+let flats = entrances * flatsPerEntrance;
+console.log('Vsego kvartir', flats);
+
+
+let vodka = 50;
+let tomatoJuice = 120;
+let lemonJuice = 10;
+let tobasco = 1;
+let worcester = 1;
+ 
+let  volume = 500;
+
+let k = (vodka + tomatoJuice + lemonJuice+ tobasco + worcester) /volume;
+
+console.log(vodka*k);
