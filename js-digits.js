35d748e9f3bb88f0d203075c5967a5f10ff89c9b
Murvat <murvat.mustafayev@mail.ru>
2023-11-15 15:38:00 +0100

    Issue with username and mail

--- js-digits.js
+++ js-digits.js
@@ -1,22 +1,12 @@
-let number= 5;
-let num2 = 2.5;
-let num3 = .5;
-let exp = 2e5;
-let expSM = 2e-3;
-expFract = 1.25e3;
-expFr3 = 1.25e-3;
+let n = 10;
 
-let dec = 9;
-let b = 0b1001;
-let oct = 0o10;
-let x = 0x1b;
+let current = 0;
+let prev = 1;
+let prevPrev  =0;
 
-
-
-let x2 = 10 + 5;
-let y = 10-5;
-let u = 10/5;
-let t = 10 % 2;
-
-console.log(t++);
-console.log(t);
+while (n--> 0) {
+    prevPrev = prev;
+    prev = current;
+    current += prevPrev;
+    console.log(current);
+}
---
+++ js-op.js
@@ -0,0 +1,9 @@
+let d = 10;
+
+if(d < 0) {
+ console.log('true')
+} else if (d === 0) {
+
+} else {
+{console.log('false')}
+}
