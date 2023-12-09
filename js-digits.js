e0ccb39e3187886c6a07aa25f6b944430bd0ad08
Murvat <murvat.mustafayev@mail.ru>
2023-12-09 16:29:09 +0100

    string&array tasks

--- js-digits.js
+++ js-digits.js
@@ -1163,3 +1163,376 @@
 //         }
 // })
 // console.log(d);
+// let str = 'hello,hi,world';
+// let a = str.split(',')
+// console.log(a);
+
+//flat fill keys
+// let a = [3,[8,7,4],5];
+// let b = a.flat(300);
+// console.log(b);
+
+// let c = [1,1,1,1,1,1,1];
+// let d  = c.fill(2,3);
+// console.log(c);
+// console.log(d);
+
+//keys
+//  let a = [3,4,5];
+//  console.log(Object.keys(a))
+// const a = [
+//         {name: 'Murvat', age:54},
+//         {name: 'Vusal', age:30},
+//         {name: 'Mamed', age:18},
+//         {name: 'Nurlan', age:20},
+//         {name: 'Xeyal', age:21},
+//         {name: 'Ibrahim', age:22},
+//         {name: 'Laman', age:19},        
+// ];
+// console.log(Object.keys(a))
+
+// let a = [1, 2, 3 ,4];
+// let sum = 0;
+// for (let i = 0;i<a.length; i++){
+//         sum = sum +a[i];
+// }
+// console.log(sum);
+
+// let b = a.reduce((prev, item, index ) =>{
+//         return prev + item
+// })
+// console.log(b);
+
+// a = [31,1,2,4,500,63,2];
+// let max = a[0];
+// for (let i = 0; i<a.length; i++){
+//         if (a[i] > max) {
+//                 max = a[i];
+//         }
+// }
+// console.log(max);
+// a = [31,-1,2,4,50,63,2];
+// let aMax = a.reduce((prev, item) =>{
+//         if(prev < item) {
+//          return item;
+//         } else {
+//                 return prev;
+//         }
+// })
+// console.log(aMax)
+
+
+//Объединение строк:
+// let firstName = "John";
+// let lastName = "Doe";
+
+// let b =  firstName +' ' +  lastName
+// console.log(b)
+// let inputString = "Hello, World!";
+// let a = inputString.length
+// console.log(inputString);
+// let sourceString = "This is a sample string";
+// let a  = sourceString.substring(0,5)
+// console.log(a)
+
+
+
+// let originalString = "Hello, world!";
+// let oldChar = "o";
+// let newChar = "a";
+
+// // Заменяем все вхождения символа oldChar на newChar в строке
+// originalString = originalString.split(oldChar).join(newChar);
+
+// // Выводим результат
+// console.log(originalString);
+// let inputString = "JavaScript is Cool";
+// console.log(inputString.toLocaleLowerCase())
+
+// let sentenceWithSpaces = "    Trim this sentence    ";
+// console.log(sentenceWithSpaces.trim());
+// // let sentenceToSplit = "This is a sample sentence";
+// // console.log(sentenceToSplit.split(' '));
+
+// let mainString = "Welcome to JavaScript";
+// let startString = "Welcome";
+// let endString = "Script";
+// let  a = () => {
+//         if(mainString.indexOf(startString)===0){
+//         return true
+// }}
+// let b = () => {
+// }
+// console.log(a())
+
+// let checkString = "Programming";
+// let targetChar = "g";
+// console.log(checkString.includes(targetChar))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let mainString = "JavaScript is fun";
+// let searchString = "un";
+
+// // let a = mainString.indexOf(searchString);
+// // console.log(a);
+// let a = mainString.includes(searchString);
+
+// console.log(a)
+// let numbersArray = [1, 2, 3, 4, 5];
+// let newElement = 6;
+// let a = numbersArray.push(newElement);
+// console.log(a)
+// console.log(numbersArray)
+
+// let stringsArray = ["apple", "banana", "cherry", "date"];
+// let a = stringsArray[1];
+// console.log(a);
+// let numbersArray = [10, 20, 30, 40, 50];
+// let indexToUpdate = 2;
+// let newValue = 35;
+// numbersArray[2]=35;
+// console.log(numbersArray)
+// let stringsArray = ["cat", "dog", "elephant", "fish"];
+// let elementToDelete = "elephant";
+// let indexToDelete = stringsArray.indexOf(elementToDelete);
+// if (indexToDelete !==-1) {
+//         stringsArray.splice(indexToDelete, 1);
+//         console.log('deleted')
+// }
+
+// Объединение строк:
+// let firstName = "John";
+// let lastName = "Doe";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// Длина строки:
+// let inputString = "Hello, World!";
+// let stringLength = inputString.length;
+// console.log(stringLength);
+
+// Извлечение подстроки:
+// let sourceString = "This is a sample string";
+// let substring = sourceString.slice(5, 10);
+// console.log(substring);
+
+// Поиск подстроки:
+// let mainString = "JavaScript is fun";
+// let searchString = "is";
+// let isSubstringPresent = mainString.includes(searchString);
+// console.log(isSubstringPresent);
+
+// Замена символов:
+// let originalString = "Hello, world!";
+// let oldChar = "o";
+// let newChar = "a";
+// let modifiedString = originalString.split(oldChar).join(newChar);
+// console.log(modifiedString);
+
+
+// Преобразование в верхний/нижний регистр:
+// let inputString = "JavaScript is Cool";
+// let upperCaseString = inputString.toUpperCase();
+// let lowerCaseString = inputString.toLowerCase();
+// console.log(upperCaseString, lowerCaseString);
+
+
+// Удаление лишних пробелов:
+// let sentenceWithSpaces = "    Trim this sentence    ";
+// let trimmedSentence = sentenceWithSpaces.trim();
+// console.log(trimmedSentence);
+
+
+// Разделение строки:
+// let sentenceToSplit = "This is a sample sentence";
+// let wordsArray = sentenceToSplit.split(" ");
+// console.log(wordsArray);\
+
+// Проверка начала и конца строки:
+// let mainString = "Welcome to JavaScript";
+// let startString = "Welcome";
+// let endString = "Script";
+// let startsWithWelcome = mainString.startsWith(startString);
+// let endsWithScript = mainString.endsWith(endString);
+// console.log(startsWithWelcome, endsWithScript);
+
+// Конкатенация строк:
+// let string1 = "Hello";
+// let string2 = ", ";
+// let string3 = "World!";
+// let concatenatedString = string1 + string2 + string3;
+// console.log(concatenatedString);
+
+
+// Преобразование числа в строку:
+// let numberValue = 42;
+// let stringValue = numberValue.toString();
+// console.log(stringValue);
+
+
+// Проверка наличия символа:
+// let checkString = "Programming";
+// let targetChar = "g";
+// let isCharPresent = checkString.includes(targetChar);
+// console.log(isCharPresent);
+
+
+// Нахождение индекса элемента:
+// let fruitsArray = ["apple", "banana", "cherry", "date"];
+// let targetFruit = "banana";
+// let fruitIndex = fruitsArray.indexOf(targetFruit);
+// console.log(fruitIndex);
+
+
+// Обратный порядок элементов:
+// let reverseArray = [1, 2, 3, 4, 5];
+// let reversedArray = reverseArray.reverse();
+// console.log(reversedArray);
+// Преобразование элементов в строки:
+// let numbersArray = [10, 20, 30, 40, 50];
+// let stringArray = numbersArray.map(String);
+// console.log(stringArray);
+
+// Проверка условия для каждого элемента:
+// let numbersArray = [5, 10, 15, 20, 25];
+// let condition = 15;
+// let isConditionMet = numbersArray.every(item => item > condition);
+// console.log(isConditionMet);
+
+
+// Нахождение первого элемента, удовлетворяющего условию:
+// let numbersArray = [5, 10, 15, 20, 25];
+// let condition = 15;
+// let firstMatch = numbersArray.find(item => item > condition);
+// console.log(firstMatch);
+
+
+// Удаление повторяющихся элементов:
+// let arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
+// let uniqueArray = Array.from(new Set(arrayWithDuplicates));
+// console.log(uniqueArray);
+
+
+// Создание подмассива на основе условия:
+// let numbersArray = [5, 10, 15, 20, 25];
+// let condition = 15;
+// let filteredArray = numbersArray.filter(item => item > condition);
+// console.log(filteredArray);
+
+// Объединение элементов в строку:
+// let stringArray = ["Hello", " ", "World", "!"];
+// let concatenatedString = stringArray.join("");
+// console.log(concatenatedString);
+
+
+// Разворачивание вложенных массивов:
+// let nestedArray = [[1, 2], [3, 4], [5, 6]];
+// let flatArray = nestedArray.flat();
+// console.log(flatArray);
+
+
+// Определение существования хотя бы одного элемента, удовлетворяющего условию:
+// let numbersArray = [5, 10, 15, 20, 25];
+// let condition = 15;
+// let isConditionMet = numbersArray.some(item => item > condition);
+// console.log(isConditionMet);
+
+
+// Определение общих элементов двух массивов:
+// let firstArray = [1, 2, 3, 4, 5];
+// let secondArray = [4, 5, 6, 7, 8];
+// let commonElements = firstArray.filter(item => secondArray.includes(item));
+// console.log(commonElements);
+
+// Создание нового массива на основе существующего:
+// let numbersArray = [1, 2, 3, 4, 5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Поиск элемента:
+// let numbersArray = [5, 10, 15, 20, 25];
+// let targetNumber = 15;
+// let a = numbersArray.find(item => item === targetNumber);
+// console.log(a);
+
+
+// Сортировка массива:
+// let stringsArray = ["orange", "apple", "banana", "grape"];
+// let a  = stringsArray.sort()
+// console.log(a)
+
+// Объединение массивов:
+// let firstArray = [1, 2, 3];
+// let secondArray = [4, 5, 6];
+// let a = firstArray.concat(secondArray);
+// console.log(a);
+
+
+// Извлечение подмассива:
+// let numbersArray = [5, 10, 15, 20, 25];
+// let startIndex = 1;
+// let length = 3;
+// let a =numbersArray.slice(startIndex, startIndex+length);
+// console.log(a)
+
+// Проверка наличия элемента:
+// let stringsArray = ["apple", "orange", "banana", "grape"];
+// let searchString = "orange";
+// let a = searchString.includes(searchString);
+// console.log(a)
+
+// Преобразование элементов:
+// let numbersArray = [2, 4, 6, 8, 10];
+// let incrementValue = 5;
+// let a = numbersArray.map(item => item + incrementValue)
+// console.log(a);
+
+// Фильтрация элементов:
+// let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let a = numbersArray.filter(item =>{
+//         if(item > 5){
+//                 return item
+//         }
+// })
+// console.log(a);
+
+
+// Нахождение суммы элементов:
+// let numbersArray = [1, 2, 3, 4, 5];
+// let sum = numbersArray.reduce((prev, item) => prev + item, 0);
+// console.log(sum);
