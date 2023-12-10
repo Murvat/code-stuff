2db7a922fb91c75fcd72e7129ad806c59567767e
Murvat <murvat.mustafayev@mail.ru>
2023-12-10 15:20:19 +0100

    ArrayJsModule

---
+++ arrayJs.js
@@ -0,0 +1,16 @@
+// Добавление/удаление элементов
+// let array =[];
+// array.pop();
+// array.push();
+// array.shift();
+// array.unshift();
+// array.splice( start[, deleteCount, elem1...elemN ])
+// array.slice([star], [end])
+// array.concat(arg1, arg2...)
+// Перебор: forEach
+// array.forEach((item, index, array){ function });
+// Поиск в массиве
+// array.indexOf(item, from)
+// array.includes(item, from)
+// 
+// 
