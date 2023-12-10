a8314b2f3fbc9c7869741826693eece0e2871ee1
Murvat <murvat.mustafayev@mail.ru>
2023-12-10 17:57:25 +0100

    ş

--- arrayJs.js
+++ arrayJs.js
@@ -1,4 +1,4 @@
-// Добавление/удаление элементов
+Добавление/удалениеэлементов
 // let array =[];
 // array.pop();
 // array.push();
@@ -7,10 +7,27 @@
 // array.splice( start[, deleteCount, elem1...elemN ])
 // array.slice([star], [end])
 // array.concat(arg1, arg2...)
-// Перебор: forEach
+Перебор:forEach
 // array.forEach((item, index, array){ function });
-// Поиск в массиве
+Поисквмассиве
 // array.indexOf(item, from)
 // array.includes(item, from)
-// n
-// 
+// array.lastIndexOf(item, from)
+// array.find(function(item, index, array){function});
+// array.findIndex()
+// array.findLastIndex()
+// array.filter(function(item,index, array){...})
+Преобразованиемассива
+// array.map(function(item, index, array){...})
+// array.sort(function)
+// array.reverse();
+// array = str.split(delim, elemCount)
+// str = array.joim(glue)
+// arrayy.reduce(
+// function(accumulator, item, index, array){...}, [initial])
+// array.some(fn)
+// array.every(fn)
+// arr.fill(value, start, end)
+// arr.copyWithin(target, start, end)
+// arr.flat(depth)
+// arr.flatMap(fn)
