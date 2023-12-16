0bedf76fe4950c29c22162086bd613c5cd3a55ae
Murvat <murvat.mustafayev@mail.ru>
2023-12-17 00:07:15 +0100

    blocks

--- String.js
+++ String.js
@@ -1,1 +1,173 @@
-String
+// String
+// charAt(index):
+// 1)Возвращает символ по указанному индексу.
+// const str = "Hello, World!";
+// const charAtIndex = str.charAt(7);
+// console.log(charAtIndex); // выводит 'W'
+// 2)charCodeAt(index):
+// Возвращает Unicode код символа по указанному индексу.
+// const str = "Hello, World!";
+// const charCodeAtIndex = str.charCodeAt(7);
+// console.log(charCodeAtIndex); // выводит 87 (Unicode для 'W')
+
+// Методы для разделения и объединения строк
+// 1)concat(str1, str2, ...):
+// Объединяет строки.
+// const str1 = "Hello";
+// const str2 = "World!";
+// const combinedString = str1.concat(", ", str2);
+// console.log(combinedString); // выводит "Hello, World!"
+// 2)split(separator, limit):
+// Разбивает строку на массив подстрок, используя указанный разделитель.
+// const str = "Hello, World!";
+// const words = str.split(", ");
+// console.log(words); // выводит ["Hello", "World!"]
+
+// Методы для проверки содержимого строки:
+// 1)includes(searchString, position):
+// Проверяет, содержится ли подстрока в строке.
+// const str = "Hello, World!";
+// const containsWorld = str.includes("World");
+// console.log(containsWorld); // выводит true
+// 2)endsWith(searchString, length):
+// Проверяет, заканчивается ли строка указанной подстрокой.
+// const str = "Hello, World!";
+// const endsWithWorld = str.endsWith("World!");
+// console.log(endsWithWorld); // выводит true
+// 3)startsWith(searchString, position):
+// Проверяет, начинается ли строка указанной подстрокой.
+// const str = "Hello, World!";
+// const startsWithHello = str.startsWith("Hello");
+// console.log(startsWithHello); // выводит true
+// 7)indexOf(searchValue, fromIndex):
+// Возвращает индекс первого вхождения подстроки.
+// const str = "Hello, World!";
+// const indexOfWorld = str.indexOf("World");
+// console.log(indexOfWorld); // выводит 7
+// 8)lastIndexOf(searchValue, fromIndex):
+// Возвращает индекс последнего вхождения подстроки.
+// const str = "Hello, World!";
+// const lastIndexOfO = str.lastIndexOf("o");
+// console.log(lastIndexOfO); // выводит 8
+// 9)length:
+// Возвращает длину строки.
+// const str = "Hello, World!";
+// const lengthOfString = str.length;
+// console.log(lengthOfString); // выводит 13
+// 10)localeCompare(compareString, locales, options):
+// Сравнивает строки в заданной локали.
+// const str1 = "apple";
+// const str2 = "banana";
+// const comparisonResult = str1.localeCompare(str2);
+// console.log(comparisonResult); // может быть отрицательным, нулевым или положительным числом
+
+// Методы для изменения строки
+// 1)toLowerCase():
+// Преобразует все символы строки в нижний регистр.
+// const str = "Hello, World!";
+// const lowerCaseString = str.toLowerCase();
+// console.log(lowerCaseString); // выводит "hello, world!"
+// 2)toUpperCase():
+// Преобразует все символы строки в верхний регистр.
+// const str = "Hello, World!";
+// const upperCaseString = str.toUpperCase();
+// console.log(upperCaseString); // выводит "HELLO, WORLD!"
+// 3)trim():
+// Удаляет пробелы в начале и в конце строки.
+// const str = "   Hello, World!   ";
+// const trimmedString = str.trim();
+// console.log(trimmedString); // выводит "Hello, World!"
+// 4)trimStart(), trimLeft():
+// Удаляет пробелы в начале строки.
+// const str = "   Hello, World!   ";
+// const trimmedString = str.trimStart();
+// console.log(trimmedString); // выводит "Hello, World!   "
+// 5)trimEnd(), trimRight():
+// Удаляет пробелы в конце строки.
+// const str = "   Hello, World!   ";
+// const trimmedString = str.trimEnd();
+// console.log(trimmedString); // выводит "   Hello, World!"
+// 6)normalize(form):
+// Нормализует строку в указанную форму Юникода.
+// const str = "é";
+// const normalizedString = str.normalize("NFD");
+// console.log(normalizedString); // выводит "é"
+// 7)repeat(count):
+// Повторяет строку указанное количество раз.
+// const str = "Hello";
+// const repeatedString = str.repeat(3);
+// console.log(repeatedString); // выводит "HelloHelloHello"
+// 8)replace(searchValue, replaceValue):
+// Заменяет подстроку новой строкой.
+// const str = "Hello, World!";
+// const replacedString = str.replace("World", "Universe");
+// console.log(replacedString); // выводит "Hello, Universe!"
+// 9)slice(startIndex, endIndex):
+// Возвращает вырезанную подстроку между указанными индексами.
+// const str = "Hello, World!";
+// const slicedString = str.slice(7, 12);
+// console.log(slicedString); // выводит "World"
+// 10)substring(startIndex, endIndex):
+// Возвращает подстроку между указанными индексами.
+// const str = "Hello, World!";
+// const substring = str.substring(7, 12);
+// console.log(substring); // выводит "World"
+// 11)substr(startIndex, length):
+// Возвращает подстроку, начиная с указанного индекса и имеющую указанную длину.
+// const str = "Hello, World!";
+// const substring = str.substr(7, 5);
+// console.log(substring); // выводит "World"
+
+// OTHER:
+//23) valueOf():
+// Возвращает примитивное значение строки.
+// const strObject = new String("Hello, World!");
+// const primitiveValue = strObject.valueOf();
+// console.log(primitiveValue); // выводит "Hello, World!"
+// 24)toString():
+// Возвращает строковое представление объекта.
+// const strObject = new String("Hello, World!");
+// const stringValue = strObject.toString();
+// console.log(stringValue); // выводит "Hello, World!"
+
+// СПОСОБЫ ПЕРЕБИРАНИЯ
+//1) Цикл for с использованием charAt и length:
+// const str = "Hello, World!";
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str.charAt(i));
+// }
+// 2)Цикл for...of:
+// const str = "Hello, World!";
+// for (const char of str) {
+//     console.log(char);
+// }
+// 3)Метод split и цикл forEach для массива символов:
+// const str = "Hello, World!";
+// const charArray = str.split('');
+// charArray.forEach(char => {
+//     console.log(char);
+// });
+// 4)Метод split и цикл for...of для массива символов:
+// const str = "Hello, World!";
+// const charArray = str.split('');
+// for (const char of charArray) {
+//     console.log(char);
+// }
+// 5)Метод Array.from и цикл forEach для массива символов:
+// const str = "Hello, World!";
+// const charArray = Array.from(str);
+// charArray.forEach(char => {
+//     console.log(char);
+// });
+// 6)Цикл while с использованием charAt и length:
+// const str = "Hello, World!";
+// let i = 0;
+// while (i < str.length) {
+//     console.log(str.charAt(i));
+//     i++;
+// }
+// 7)Цикл for с использованием charCodeAt и length:
+// const str = "Hello, World!";
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str.charCodeAt(i));
+// }
--- number.js
+++ number.js
@@ -1,1 +1,160 @@
-Numbers
+// Numbers
+// Округление и преобразование:
+// Math.round(): Округляет число до ближайшего целого.
+// let roundedNumber = Math.round(4.7); // 5
+
+// Math.floor(): Округляет число вниз до ближайшего целого.
+// let flooredNumber = Math.floor(4.7); // 4
+
+// Math.ceil(): Округляет число вверх до ближайшего целого.
+// let ceiledNumber = Math.ceil(4.3); // 5
+
+// toFixed(): Округляет число до указанного количества знаков после запятой и возвращает строку.
+// let fixedNumber = (4.567).toFixed(2); // "4.57"
+
+// Генерация случайных чисел:
+// Math.random(): Возвращает случайное число от 0 (включительно) до 1 (исключительно).
+// let randomNum = Math.random();
+
+// Генерация случайного целого числа в диапазоне:
+// let randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Работа с числами:
+
+// parseInt(): Преобразует строку в целое число.
+// let intNumber = parseInt("123"); // 123
+
+// parseFloat(): Преобразует строку в число с плавающей точкой.
+// let floatNumber = parseFloat("3.14"); // 3.14
+
+// isNaN(): Проверяет, является ли значение не числом.
+// let isNotANumber = isNaN("abc"); // true
+
+// Операции:
+// Оператор + для сложения чисел:
+// let sum = 2 + 3; // 5
+
+// Оператор - для вычитания чисел:
+// let difference = 5 - 2; // 3
+
+// Оператор * для умножения чисел:
+// let product = 4 * 2; // 8
+
+// Оператор / для деления чисел:
+// let quotient = 8 / 2; // 4
+
+// Проверка свойств чисел:
+// isFinite(): Проверяет, является ли значение конечным числом.
+// let finiteNumber = isFinite(42); // true
+
+// isInteger(): Проверяет, является ли число целым.
+// let isInteger = Number.isInteger(5); // true
+
+// isSafeInteger(): Проверяет, является ли число "безопасным" целым числом.
+// let safeInteger = Number.isSafeInteger(9007199254740992); // false
+
+// Математические операции:
+// Math.abs(): Возвращает абсолютное значение числа.
+// let absoluteValue = Math.abs(-5); // 5
+
+// Math.pow(): Возводит число в указанную степень.
+// let power = Math.pow(2, 3); // 8
+
+// Math.sqrt(): Извлекает квадратный корень числа.
+// let squareRoot = Math.sqrt(25); // 5
+
+// Math.log(): Возвращает натуральный логарифм числа.
+// let naturalLog = Math.log(2.71828); // 1
+
+// Конвертация в различные системы счисления:
+
+// parseInt() для конвертации в двоичную систему:
+// let binaryNumber = parseInt("1010", 2); // 10
+
+// parseInt() для конвертации в восьмеричную систему:
+// let octalNumber = parseInt("12", 8); // 10
+// parseInt() для конвертации в шестнадцатеричную систему:
+// let hexNumber = parseInt("A", 16); // 10
+
+// Проверка на четность/нечетность:
+// Проверка на четность:
+// let isEven = num % 2 === 0; // true if num is even
+// Проверка на нечетность:
+// let isOdd = num % 2 !== 0; // true if num is odd
+
+// Number.toFixed():
+// Округляет число до указанного количества знаков после запятой и возвращает строку.
+// let fixedNumber = (7.891).toFixed(1); // "7.9"
+
+// Number.toPrecision():
+// Форматирует число в виде строки в зависимости от указанной точности.
+// let precisionNumber = (123.456).toPrecision(4); // "123.5"
+
+// Number.toExponential():
+// Преобразует число в экспоненциальную запись.
+// let exponentialNumber = (12345).toExponential(); // "1.2345e+4"
+
+// Number.isNaN():
+// Проверяет, является ли значение NaN (не числом).
+// let isNan = Number.isNaN("abc"); // false
+
+// Number.isFinite():
+// Проверяет, является ли значение конечным числом, и не является ли NaN или бесконечностью.
+// let isFiniteNumber = Number.isFinite(42); // true
+
+// Number.parseFloat():
+// Аналогично глобальной функции parseFloat, но является статическим методом Number.
+// let parsedFloat = Number.parseFloat("3.14"); // 3.14
+
+// Number.MAX_VALUE и Number.MIN_VALUE:
+// Представляют максимальное и минимальное положительные значения типа "число".
+// let maxVal = Number.MAX_VALUE;
+// let minVal = Number.MIN_VALUE;
+
+// Math.abs() для поиска расстояния между двумя точками:
+// Расстояние между точками (x1, y1) и (x2, y2) в декартовой системе координат.
+// let distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+// Math.random() для получения случайного числа в диапазоне [0, 1):
+// Получение случайного числа в диапазоне от 0 (включительно) до 1 (исключительно).
+// let randomNumInRange = Math.random() * (max - min) + min;
+
+// Math.sign():
+// Возвращает знак числа, указывающий на то, является ли число положительным, отрицательным или нулем.
+// let sign = Math.sign(-7); // -1
+
+// Math.trunc():
+// Усекает десятичную часть числа, возвращая только целую часть.
+// let truncatedNumber = Math.trunc(5.78); // 5
+
+// Math.cbrt():
+// Возвращает кубический корень числа.
+// let cubicRoot = Math.cbrt(27); // 3
+
+// Number.EPSILON:
+// Минимальное расстояние между двумя числами, при котором они считаются различными.
+// let epsilon = Number.EPSILON;
+
+// Цикл for для перебора числовой последовательности:
+// for (let i = 0; i < 5; i++) {
+//     console.log(i); // 0, 1, 2, 3, 4
+// }
+
+//  Цикл for для перебора чисел в обратном порядке:
+// for (let i = 5; i > 0; i--) {
+//     console.log(i); // 5, 4, 3, 2, 1
+// }
+
+// Цикл while для перебора чисел:
+// let i = 0;
+// while (i < 5) {
+//     console.log(i); // 0, 1, 2, 3, 4
+//     i++;
+// }
+
+// Цикл do-while для перебора чисел:
+// let i = 0;
+// do {
+//     console.log(i); // 0, 1, 2, 3, 4
+//     i++;
+// } while (i < 5);
