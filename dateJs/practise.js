e1d57e7ef193c23249b7c2682228a7fe6bdb0577
Murvat <murvat.mustafayev@mail.ru>
2023-12-17 19:59:57 +0100

    dateJs

---
+++ dateJs/practise.js
@@ -0,0 +1,66 @@
+
+// let now = new Date()
+// console.log(now);
+
+// let now2 = new Date(0)
+// console.log(now2);
+
+// let now3 = new Date(24 * 3600 * 1000)
+// console.log(now3)
+
+// let now4 = new Date('2021-01-26')
+// console.log(now4)
+
+
+// let counter = now4.getFullYear()
+// console.log(counter)
+// // let counter2 = now4.getMonth()
+// let counter2 = now4.getDay()
+// console.log(counter2)
+
+// let date = new Date(2013, 1, 20, 3, 12)
+// console.log(date)
+
+// let weekDays =['Monday','Tuesday','Wednesday','thursday','Friday','Sunday','Saturday']
+
+// function getWeekDay(date){
+//     if(date.getDay()===0){console.log(weekDays[0])}
+//     else 
+//     if(date.getDay()===1){console.log(weekDays[1])}
+//     else
+//     if(date.getDay()===2){console.log(weekDays[2])}
+//     else
+//     if(date.getDay()===3){console.log(weekDays[3])}
+//     else
+//     if(date.getDay()===4){console.log(weekDays[4])}
+//     else        
+//     if(date.getDay()===5){console.log(weekDays[5])}
+//     else
+//     if(date.getDay()===6){console.log(weekDays[6])}
+// }
+// let date = new Date(2023, 0, 19)
+// getWeekDay(date)
+
+// let weekDays =['Monday','Tuesday','Wednesday','thursday','Friday','Sunday','Saturday']
+
+// function getWeekDay(date){
+//     console.log(weekDays[date.getDay()])
+// }
+// let date = new Date(2023, 0, 9)
+// getWeekDay(date)
+
+// function getLD(year, month){
+//     let date = new Date(year, month+1, 0);
+//     console.log(date.getDate());
+// }
+// getLD(2012, 0)
+
+// function getST(){
+//     let now = new Date();
+//     let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+//     let diff = now-today;
+//     console.log( Math.round(diff/ 1000))
+// }
+// getST()
+
+
