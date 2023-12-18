c791758d11483cb4bf89767db661b3ff518f3da2
Murvat <murvat.mustafayev@mail.ru>
2023-12-19 00:17:19 +0100

    .

---
+++ Json/json.js
@@ -0,0 +1,37 @@
+// let user = {
+//     name: "John",
+//     age: 30,
+//     toString() {
+//         return `{
+//             name: "${this.name}",
+//             age: ${this.age}
+//         }`;
+//     }
+// };
+
+// console.log(user);
+
+// let student = {
+//     name:'Murvat',
+//     age: 30,
+//     isAdmin: false,
+//     courses:['html', 'css', 'reactJS'],
+//     wife:null
+// };
+
+// let json = JSON.stringify(student);
+
+// console.log(typeof json)
+// console.log(json);
+
+
+
+// let user  ={
+//     sayHi(){
+//         alert("Hello");
+//     },
+
+//     [Symbol('id')]: 123,
+//     something: undefined
+// };
+// console.log(JSON.stringify(user))
