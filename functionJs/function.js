ab13d755c8433384ecd3c0e286229571521b0183
Murvat <murvat.mustafayev@mail.ru>
2023-12-23 20:25:51 +0100

    recursia

--- functionJs/function.js
+++ functionJs/function.js
@@ -49,5 +49,53 @@ Function
 // }
 
 // // Example usage:
-// console.log(fibonacci(10)); // Output: 8 (Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, ...)
+// console.log(fibonacci(10)); // Output: 8 (Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, ...
+// function f2() {
+//     let out = '';
+//     for(let i=1;i <=200;i++){
+//         out+= i + ' ';
+//     }
+//     console.log(out)
+// }
+// f2()
 
+// let i = 0;
+// let out = '';
+// function f3(){
+//     i++;
+//     out += i+ ' ';
+//     if(i > 30) return;
+//     f3()
+// }
+// f3();
+// console.log(out)
+
+function randomInteger(min, max) {
+    let rand = min + Math.random() *(max +1 -min);
+    return Math.floor(rand)
+}
+
+// let s1 = 0;
+// function moneyRecursion(){
+//     let m = randomInteger(0, 100);
+//     console.log('add: ' + m);
+//     s1 += m;
+//     console.log('sum: ' + s1)
+//     if (s1>= 300) return;
+//     moneyRecursion();
+// }
+// moneyRecursion();
+
+// function moneyCycle(){
+//     let s2 = 0;
+//     for (let i=0; true; i++){
+//         let m = randomInteger(0, 100);
+//         console.log('add: ' + m );
+//         s2+= m;
+//         console.log('sum: '+ s2);
+//         if(s2 >= 300) return;
+//     }
+// }
+// moneyCycle()
+
+// 
