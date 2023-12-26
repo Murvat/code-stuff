94d7dbfd4638d8024392e9537af3924d433201c9
Murvat <murvat.mustafayev@mail.ru>
2023-12-26 19:01:00 +0100

    cashe

--- functionJs/function.js
+++ functionJs/function.js
@@ -262,3 +262,29 @@ Function
 // let consumer  = {name: 'Kyrill'}
 
 // goUp.call(consumer)
+
+// let worker = {
+//     someMethod() {
+//         return 1;
+//     },
+
+//     slow(x) { 
+//       console.log('Called with' + x);
+//          return x * this.someMethod();
+// }
+// };
+// function cachingDecorator(func){
+//     let cache = new Map();
+//     return function(x){
+//         if(cache.has(x)){
+//             return cache.get(x);
+//         }
+//         let result = func.call(this, x);
+//         cache.set(x, result)
+//         return result;
+//     }
+// }
+
+// worker.slow = cachingDecorator(worker.slow);
+// console.log(worker.slow(2));
+// console.log(worker.slow(2));
