45bdcd197e54f99424a8f98bd3de0616107329cf
Murvat <murvat.mustafayev@mail.ru>
2024-01-02 18:38:53 +0100

    p.

--- Object/objProto.js
+++ Object/objProto.js
@@ -1,12 +1,16 @@
 let animal  ={
     eats: true,
+    walk(){
+         console.log('Animal Walk')
+    }
 }
 let rabbit = {
     jumps: true,
 }
 
 rabbit.__proto__=animal;
-console.log(rabbit);
-console.log(animal);
-console.log(rabbit.eats)
-console.log(animal.jumps)
+// console.log(rabbit);
+// console.log(animal);
+// console.log(rabbit.eats)
+// console.log(animal.jumps)
+rabbit.walk()
