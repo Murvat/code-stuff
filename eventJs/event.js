ed92c80c0a6dbd7fcd59e7e72c33c92079bf71fe
Murvat <murvat.mustafayev@mail.ru>
2024-01-07 18:40:43 +0100

    .

--- eventJs/event.js
+++ eventJs/event.js
@@ -1,1 +1,19 @@
-event
+// event
+
+
+
+// let json= '{"name": "John", "age":30}';
+// let user = JSON.parse(json);
+// console.log(user.name);
+// let json= '{Mess codedede}';
+// try {
+// let user = JSON.parse(json);
+// console.log(user.name);}
+// catch(e){
+//     console.log('There is a mistake')
+// }
+function loadScript(){
+    let script = document.createElement('script');
+    script.src=src;
+    document.head.append(script);
+}
