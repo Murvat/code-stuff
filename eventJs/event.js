16d34c7f78374388b10bcf34f2b30c1d476b1413
Murvat <murvat.mustafayev@mail.ru>
2024-01-14 13:24:22 +0100

    .

--- eventJs/event.js
+++ eventJs/event.js
@@ -33,5 +33,41 @@
 
 
 
+// function* genFn(x){
+//     return x *2;
+// }
+
+// console.log('genFn= ', [genFn]);
+// console.log('genFn.toString()= ', [genFn.toString()]);
+// console.log('typeof(genfn)', typeof(genFn));
+// const fnProto = Object.getPrototypeOf(genFn);
+// console.log('fnProto.constructor.name = ', fnProto.constructor.name);
+
+// console.log('typeof(genFn(5))= ', typeof(genFn(5)));
+// console.log('genFn(5).toString()=', genFn(5).toString());
+// const genProto = Object.getPrototypeOf(genFn(5));
+// console.log('genProto= ', genProto);
+// console.log('genProto[Symbol.Iterator=', genProto[Symbol.iterator]);
+// console.log('genFn(5)=', genFn(5));
+
+// class Multiplier {
+//     constructor(k) {
+//         this.value = k;
+//     }
+//     * genMethod(a){
+//         this.value = a* this.value;
+//         return a* this.value;
+
+//     }
+// } 
+// const m1 = new Multiplier(3);
+// console.log(m1.genMethod(2).next())
+// console.log('m1.genMethod(5).next()=', m1.genMethod(5).next());
+// console.log(m1.genMethod(2).next())
+// console.log(m1.genMethod(2).next())
+// console.log(m1.genMethod(2).next())
+// console.log(m1.genMethod(2).next())
+
+
 
 
--- generatorJs/generatorJs.js
+++ generatorJs/generatorJs.js
@@ -36,14 +36,88 @@
 // }
 
 
-function* genFn(x){
-    return x *2;
+// function* genFn(x){
+//     return x *2;
+// }
+
+// console.log('genFn= ', [genFn]);
+// console.log('genFn.toString()= ', [genFn.toString()]);
+// console.log('typeof(genfn)', typeof(genFn));
+// const fnProto = Object.getPrototypeOf(genFn);
+// console.log('fnProto.constructor.name = ', fnProto.constructor.name);
+// class Multiplier {
+//     constructor(k) {
+//         this.value = k;
+//     }
+//     * genMethod(a){
+//         this.value = a* this.value;
+//         return a* this.value;
+
+//     }
+// } 
+// const m1 = new Multiplier(3);
+// console.log(m1.genMethod(2).next())
+// console.log('m1.genMethod(5).next()=', m1.genMethod(5).next());
+// console.log(m1.genMethod(2).next())
+// console.log(m1.genMethod(2).next())
+// console.log(m1.genMethod(2).next())
+// console.log(m1.genMethod(2).next())
+
+// function* counter(begin, end, delta = 1) {
+//     let value = begin;
+//     while (end> value) {
+//         value += delta;
+//         yield value;
+//     }
+// }
+
+// const c = counter(0, 30, 12);
+// const val1 = c.next();
+// const val2 = c.next();
+// const val3 = c.next();
+// const val4 = c.next();
+// console.log({c, val1, val2, val3, val4});
+
+
+// function* ids(...args) {
+//     let i =0;
+//     while(args.length > i){
+//         const id = args[i++];
+//         if(id === undefined) return -1;
+//         yield id;
+//     }
+// }
+
+// const id = ids(1011, 1078, 1292, 1731, undefined, 1501, 1550);
+// let val;
+// do {
+//     val =id.next();
+//     console.log({val});
+// } while (!val.done);
+// function* genfn(){
+//     yield* [10, 20, 30];
+// }
+// const c= genfn();
+// const val1 = c.next();
+// const val2 = c.next();
+// const val3 = c.next();
+// console.log({c, val1, val2, val3,val4})
+
+function* gen1(){
+    yield 10;
+    yield 20;
+    yield 30;
 }
 
-console.log('genFn= ', [genFn]);
-console.log('genFn.toString()= ', [genFn.toString()]);
-console.log('typeof(genfn)', typeof(genFn));
-const fnProto = Object.getPrototypeOf(genFn);
-console.log('fnProto.constructor.name = ', fnProto.constructor.name);
+function* gen2(){
+    yield 40;
+    yield 50;
+    yield 60;
+}
 
+function* genFn(){
+    yield* gen1();
+    yield* gen2();
+}
 
+console.log([...genFn()]);
