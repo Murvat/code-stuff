e18c1b9bc22950c961ac5087b3af929fb18971b9
Murvat <murvat.mustafayev@mail.ru>
2024-01-16 23:23:31 +0100

    .

--- proxyJs/proxy.js
+++ proxyJs/proxy.js
@@ -83,3 +83,77 @@
 // })
 
 // console.log(Object.keys(user))
+
+
+
+// let user = {
+//     name: 'Vlad',
+//     _password: "***"
+// };
+
+// user = new Proxy(user, {
+//     get(target, prop){
+//         if(prop.startsWith('-')){
+//             throw new Error('Rejected')
+//         } else {
+//             let value = target[prop];
+//             return (typeof value === 'function') ? value.bind(target):value;
+//         }
+//     },
+//     set(target, prop, val){
+//         if(prop.startsWith('-')){
+//             throw new Error('RejectedForChange')
+//         } else {
+//             target[prop]= val;
+//             return true;
+//         }
+//     },
+//     deleteProperty(target, prop){
+//         if (prop.startsWith('-')){
+//             throw new Error('RejectedTOO')
+//         } else {
+//             delete target[prop];
+//             return true;
+//         }
+//     },
+//     ownKeys(target){
+//         return Object.keys(target).filter(key => !key.startsWith('-'))
+//     }
+// })
+// try {
+//     console.log(user._password);
+// }catch(e){console.log(e.message);}
+// try {
+//     user._password = "test"
+// }catch(e){console.log(e.message);}
+// try {
+//      delete user._password
+// }catch(e){console.log(e.message);}
+// try {
+//     console.log(user._password);
+// }catch(e){console.log(e.message);}
+// try {
+//     console.log(user._password);
+// }catch(e){console.log(e.message);}
+
+
+
+const targetObject = {
+    name: "John",
+    age: 25
+};
+
+const handler = {
+    get: function(target, prop){
+        console.log(`reading of property ${prop} `)
+    return target[prop];
+    },
+    set: function(target, prop, value) {
+        console.log(`Setting new value of prtoperty ${prop} to ${value}`)
+    target[prop] = value;
+    }
+};
+
+const proxyObject = new Proxy(targetObject, handler)
+console.log(proxyObject.name);
+proxyObject.age = 26;
