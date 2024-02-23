e4dff6d62a7f8ae80ed69a81245398fa22cf0549
Murvat <murvat.mustafayev@mail.ru>
2024-02-23 21:44:08 +0100

    /

--- Loop/whi.js
+++ Loop/whi.js
@@ -1,8 +1,20 @@
-let n = 1;
-while(n<=10){
-    console.log(n);
-    n++;
-};
-function sad(){
-    console.log('sdfsd')
+// let n = 1;
+// while(n<=10){
+//     console.log(n);
+//     n++;
+// };
+// for(let i = 1; i<=5; i++){
+//     for(let j = 1; j<=10;j++){
+//         console.log(i ,'x', j ,'=', i*j)
+//     }
+// }
+
+function getSpecialNumbers(from, to){
+    let specialNumbers = [];
+    for(let i = from; i<= to; i++){
+        if(i%2===1){
+        specialNumbers.push(i)}
+    }
+    console.log(specialNumbers);
 }
+getSpecialNumbers(40, 60);
