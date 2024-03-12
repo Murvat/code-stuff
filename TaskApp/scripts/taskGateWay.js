338bd7619841256cac3789a8c17b449f4b28699d
Murvat <murvat.mustafayev@mail.ru>
2024-03-12 22:45:24 +0100

    .

--- TaskApp/scripts/taskGateWay.js
+++ TaskApp/scripts/taskGateWay.js
@@ -1,5 +1,30 @@
-const baseUrl ='';
+// const successRequest = Promise.resolve({name:'Tom'});
 
-export const getTasksList = ()=>{
-    
-}
+// successRequest
+// .then(data => {
+//     console.log(data)
+//     throw new Error('Unexpected error');
+// })
+// .catch(err=>{
+//     console.error(err)
+// });
+
+
+// const failedRequest = Promise.reject(new Error('Fail'));
+
+// failedRequest
+// .then(data => {
+//     console.log(data);
+// })
+// .catch(err => {
+//     console.warn(err.message);
+// })
+// .then(data=>{
+//     console.log(data)
+// })
+
+// fetch('https://api.com')
+// .then(response =>{
+//     return response.json();
+// })
+// .then(data=>console.log(data))
