7d1322f548613c251c4fc73138a40b26c44bd941
Murvat <murvat.mustafayev@mail.ru>
2024-03-18 18:45:42 +0100

    .

--- TaskApp/scripts/storage.js
+++ TaskApp/scripts/storage.js
@@ -1,5 +1,5 @@
-export const setItem = (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value))
-};
+// export const setItem = (key, value) => {
+//     localStorage.setItem(key, JSON.stringify(value))
+// };
 
-export const getItem = key => JSON.parse(key);
+// export const getItem = key => JSON.parse(key);
