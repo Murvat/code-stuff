889eb3ba8bef0ace243c2105a4ee17e463b08005
Murvat <murvat.mustafayev@mail.ru>
2024-03-19 21:11:16 +0100

    .

--- TaskApp/scripts/toDoList.js
+++ TaskApp/scripts/toDoList.js
@@ -1,10 +1,10 @@
-import { onCreate } from "./createTask";
-import{onToggle} from './update';
+// import { onCreate } from "./createTask";
+// import{onToggle} from './update';
 
-export const initTodoListHandlers = () =>{
-    const createBtnElem = document.querySelector('.create-task-btn');
-    createBtnElem.addEventListener('click',onCreate)
+// export const initTodoListHandlers = () =>{
+//     const createBtnElem = document.querySelector('.create-task-btn');
+//     createBtnElem.addEventListener('click',onCreate)
 
-    const toDoListElem = document.querySelector('.list');
-    toDoListElem.addEventListener('click', onToggle)
-}
+//     const toDoListElem = document.querySelector('.list');
+//     toDoListElem.addEventListener('click', onToggle)
+// }
