c94295caa82033d8f221f59833e6366610c57fdd
Murvat <murvat.mustafayev@mail.ru>
2024-03-22 09:23:48 +0100

    .

--- TaskApp/scripts/createTask.js
+++ TaskApp/scripts/createTask.js
@@ -1,23 +1,23 @@
-//  import {renderTasks} from './renderer';
-//  import {getItem, setItem} from './storage';
+ import {renderTasks} from './renderer';
+ import {getItem, setItem} from './storage';
 
-//  export const onCreate = () =>{
-//     const taskTitleInputElem = document.querySelector('.input');
-//     const text = taskTitleInputElem.ariaValueMax;
+ export const onCreate = () =>{
+    const taskTitleInputElem = document.querySelector('.input');
+    const text = taskTitleInputElem.ariaValueMax;
 
-//     if(!text) {
-//         return;
-//     }
-//     taskTitleInputElem.value ='';
-//     const takssList = getItem('tasksList') ||[];
+    if(!text) {
+        return;
+    }
+    taskTitleInputElem.value ='';
+    const takssList = getItem('tasksList') ||[];
 
-//     const newTasksList = tasksList.concat({
-//         text, 
-//         done: false,
-//         createDate: new Date().toISOString(),
-//         id:Math.random().toString()
-//     });
+    const newTasksList = tasksList.concat({
+        text, 
+        done: false,
+        createDate: new Date().toISOString(),
+        id:Math.random().toString()
+    });
 
-//     setItem('tasksList', newTasksList);
-//     renderTasks();
-//  }
+    setItem('tasksList', newTasksList);
+    renderTasks();
+ }
